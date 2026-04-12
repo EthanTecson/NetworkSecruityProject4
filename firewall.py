93b090f4d97103fa7c1dd60e5047013f6c3797bb
Daniel Ziabicki <ziambida@fastmail.fm>
2026-04-12 14:01:17 -0400

    log LOG packets

--- firewall.py
+++ firewall.py
@@ -1,10 +1,13 @@
 from rule_engine import RuleEngine
 from state_table import StateTable
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 class Firewall:
     def __init__(self, rules):
-        self.rule_engine = RuleEngine(rules)
+        self.rule_engine = rules
         self.state_table = StateTable()
 
     def process_packet(self, packet):
@@ -12,6 +15,10 @@ class Firewall:
             return "ALLOW"
 
         action = self.rule_engine.match(packet)
+        
+        # include logging for LOG packets
+        if action == "LOG":
+            logging.info(packet)
 
         if packet["protocol"] == "TCP":
             self.state_table.update(packet, action)
