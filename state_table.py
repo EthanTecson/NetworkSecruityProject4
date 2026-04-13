3dfe60f5f5c214952d349b0e1f0c3f143bfcba9a
Daniel Ziabicki <ziambida@fastmail.fm>
2026-04-13 12:51:20 -0400

    some changes

--- state_table.py
+++ state_table.py
@@ -1,6 +1,7 @@
 class StateTable:
     def __init__(self):
         self.connections = set()
+        self.tcp_states = {}
 
     def _key(self, packet):
         return (
@@ -28,8 +29,28 @@ class StateTable:
 
         return forward in self.connections or backward in self.connections
 
+    def update_TCP_connections(self, packet, packet_type):
+        packetF = f"{packet['src_ip']} {packet['src_port']} {packet['dst_ip']} {packet['dst_port']}"
+        packetB = f"{packet['dst_ip']} {packet['dst_port']} {packet['src_ip']} {packet['src_port']}"
+        
+        if packetF in self.tcp_states.keys():
+            #if self.tcp_states[packetF] == "SYN" and packet_type
+
+        #self.tcp_states[packetF] = 
+            pass
 
     def update(self, packet, action):
         # TODO: implement TCP state tracking
-        if action == "ALLOW":
+        
+        packet_type = ""
+        if "SYN" in packet["flags"] and "ACK" in packet["flags"]:
+            packet_type = "SYN-ACK"
+        elif "SYN" in packet["flags"]:
+            packet_type = "SYN"
+        elif "ACK" in packet["flags"]:
+            packet_type = "ACK"
+        
+        
+    
+        if action == "ALLOW" and packet_type == "SYN":
             self.connections.add(self._key(packet))
